fcc7b82108726f4a9c70add3d481234f8f8a2936
mvrakesh341 <89510137+mvrakesh341@users.noreply.github.com>
2021-08-25 14:27:02 +0530

    Add files via upload

---
+++ basic calculator.js
@@ -0,0 +1,11 @@
+let a=10
+let b=34
+console.log("a=10,b=34")
+sum=a+b
+diff=a-b
+multiply=a*b
+div=a/b
+console.log("sum of two numbers is :",sum)
+console.log("difference of two numbers is :",diff)
+console.log("multiply of two numbers is :",multiply)
+console.log("division of two numbers is:",div)
